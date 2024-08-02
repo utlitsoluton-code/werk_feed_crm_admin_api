b1efa9f9ad0c0b6bf102793427a151fe32a90080
niraj-inquiry <niraj.inquiry@gmail.com>
2024-08-02 14:45:06 +0530

    cash sale api created

--- module/common/products/routes/productRoute.js
+++ module/common/products/routes/productRoute.js
@@ -1,17 +1,18 @@
+const { jwtAdminVerify } = require('../../../../helper/authHandler')
 const { createProduct, getAllProduct, getProductById, updateProduct, createProductCategory, getProductCategory, updateProductCategory, deleteProductCategory, deleteProduct } = require('../controllers/productController')
 
 const productRoutes = require('express').Router()
 
-productRoutes.post('/create-product',createProduct)
-productRoutes.get('/get-all-product',getAllProduct)
-productRoutes.get('/get-product-by-id',getProductById)
-productRoutes.patch('/update-product',updateProduct)
-productRoutes.delete('/delete-product',deleteProduct)
+productRoutes.post('/create-product',jwtAdminVerify,createProduct)
+productRoutes.get('/get-all-product',jwtAdminVerify,getAllProduct)
+productRoutes.get('/get-product-by-id',jwtAdminVerify,getProductById)
+productRoutes.patch('/update-product',jwtAdminVerify,updateProduct)
+productRoutes.delete('/delete-product',jwtAdminVerify,deleteProduct)
 
-productRoutes.post('/create-product-category',createProductCategory)
-productRoutes.get('/get-all-product-category',getProductCategory)
-productRoutes.patch('/update-product-category',updateProductCategory)
-productRoutes.delete('/delete-product-category',deleteProductCategory)
+productRoutes.post('/create-product-category',jwtAdminVerify,createProductCategory)
+productRoutes.get('/get-all-product-category',jwtAdminVerify,getProductCategory)
+productRoutes.patch('/update-product-category',jwtAdminVerify,updateProductCategory)
+productRoutes.delete('/delete-product-category',jwtAdminVerify,deleteProductCategory)
 
 module.exports = productRoutes
 
