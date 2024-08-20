3fec00a33a1b4d3a83a502e7e56a4abcf7a69cbe
niraj-inquiry <niraj.inquiry@gmail.com>
2024-08-20 13:28:01 +0530

    new dist file added for update

---
+++ public/dist/assets/UserDetails-7bdd35c2.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-98bf78b2.js";const t=()=>s.jsx("div",{children:"UserDetails"});export{t as default};
---
+++ public/dist/assets/use-router-5ef7f58a.js
@@ -0,0 +1,1 @@
+import{a1 as o,r as a}from"./index-98bf78b2.js";function s(){const r=o();return a.useMemo(()=>({back:()=>r(-1),forward:()=>r(1),reload:()=>window.location.reload(),push:e=>r(e),replace:e=>r(e,{replace:!0})}),[r])}export{s as u};
